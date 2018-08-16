56336196af2abf8ceefb75c5a582c7fcf4120520
mimof <mimof@LAPTOP-FOPQSPDM>
2018-08-16 16:30:55 +0800

    建立普通的java project项目 采用独立Tomcat运行。data文件夹中是数据库表结构文件

---
+++ data/tmall.sql
@@ -0,0 +1,197 @@
+﻿# Host: 127.0.0.1  (Version: 5.5.15)
+# Date: 2018-08-16 16:19:58
+# Generator: MySQL-Front 5.3  (Build 4.269)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "category"
+#
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "category"
+#
+
+
+#
+# Structure for table "product"
+#
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `subTitle` varchar(255) DEFAULT NULL,
+  `orignalPrice` float DEFAULT NULL,
+  `promotePrice` float DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `createDate` datetime DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_category` (`cid`),
+  CONSTRAINT `fk_product_category` FOREIGN KEY (`cid`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "product"
+#
+
+
+#
+# Structure for table "productimage"
+#
+
+DROP TABLE IF EXISTS `productimage`;
+CREATE TABLE `productimage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_productimage_product` (`pid`),
+  CONSTRAINT `fk_productimage_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "productimage"
+#
+
+
+#
+# Structure for table "property"
+#
+
+DROP TABLE IF EXISTS `property`;
+CREATE TABLE `property` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cid` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_property_category` (`cid`),
+  CONSTRAINT `fk_property_category` FOREIGN KEY (`cid`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "property"
+#
+
+
+#
+# Structure for table "propertyvalue"
+#
+
+DROP TABLE IF EXISTS `propertyvalue`;
+CREATE TABLE `propertyvalue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(255) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `ptid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_propertyvalue_property` (`ptid`),
+  KEY `fk_propertyvalue_product` (`pid`),
+  CONSTRAINT `fk_propertyvalue_property` FOREIGN KEY (`ptid`) REFERENCES `property` (`id`),
+  CONSTRAINT `fk_propertyvalue_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "propertyvalue"
+#
+
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+
+#
+# Structure for table "review"
+#
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(4000) DEFAULT NULL,
+  `createDate` datetime DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_review_product` (`pid`),
+  KEY `fk_review_user` (`uid`),
+  CONSTRAINT `fk_review_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`),
+  CONSTRAINT `fk_review_user` FOREIGN KEY (`uid`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "review"
+#
+
+
+#
+# Structure for table "orderitem"
+#
+
+DROP TABLE IF EXISTS `orderitem`;
+CREATE TABLE `orderitem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `oid` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_orderitem_user` (`uid`),
+  KEY `fk_orderitem_product` (`pid`),
+  CONSTRAINT `fk_orderitem_user` FOREIGN KEY (`uid`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_orderitem_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "orderitem"
+#
+
+
+#
+# Structure for table "order_"
+#
+
+DROP TABLE IF EXISTS `order_`;
+CREATE TABLE `order_` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orderCode` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `post` varchar(255) DEFAULT NULL,
+  `receiver` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `userMessage` varchar(255) DEFAULT NULL,
+  `createDate` datetime DEFAULT NULL,
+  `payDate` datetime DEFAULT NULL,
+  `deliveryDate` datetime DEFAULT NULL,
+  `confirmDate` datetime DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_order_user` (`uid`),
+  CONSTRAINT `fk_order_user` FOREIGN KEY (`uid`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "order_"
+#
+
